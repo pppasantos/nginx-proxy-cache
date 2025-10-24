2a590dd029e7eae1e2b2b8d8fe3c9927c441a972
Pedro Amarante <pppasantos@gmail.com>
2025-10-24 11:34:08 -0300

    added request headers and response headers

--- lua/response_headers.lua
+++ lua/response_headers.lua
@@ -1,17 +1,17 @@
-local _M = {}
-
-function _M.capture_request_headers()
-    local headers = ngx.req.get_headers()
+function _M.capture()
+    local headers = ngx.resp.get_headers()
     local headers_str = ""
-
+ 
     for k, v in pairs(headers) do
         if type(v) == "table" then
             v = table.concat(v, ", ")
         end
         headers_str = headers_str .. k .. ": " .. v .. "; "
     end
-
-    ngx.var.req_headers = headers_str
+ 
+    ngx.var.resp_headers = headers_str
+ 
+    return headers_str
 end
-
+ 
 return _M
